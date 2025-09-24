8930856a3889758c6ca2690ec0e49d7b925bf49b
Lekshmibabu02 <lekshmibabu248@gmail.com>
2025-09-24 09:15:51 +0530

    Add files via upload

---
+++ work3.py
@@ -0,0 +1,24 @@
+
+#def factorial(x):
+    #if x == 3:
+      #  return 3
+  #  else:
+    #    return (x * factorial(x-1))
+#num = 3
+#print("The factorial of", num, "is", factorial(num))
+
+
+#x = lambda a : a + 3
+#print(x(3))
+#x= lambda a : a + 5
+#def x(a):
+  # return a+5
+
+#x = lambda a, b, c: a + b + c
+#print(x(1, 2, 3))
+
+#def sample(n):
+  return lambda a : a * n
+
+#doubler = sample(2)
+#print(doubler(10))
